30a8b5f493ae094f0be9d22ab186a06ad8168601
Adem Saka <101714041+AdemSaka61@users.noreply.github.com>
2022-05-07 19:08:35 +0300

    Add files via upload

---
+++ palindromStrings.java
@@ -0,0 +1,20 @@
+public class palindromStrings {
+    static boolean isPalindrom(String str){
+        int i = 0, j = str.length()-1;
+
+        while (i < j){
+            if (str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrom("adem"));
+        System.out.println(isPalindrom("aba"));
+
+    }
+}
